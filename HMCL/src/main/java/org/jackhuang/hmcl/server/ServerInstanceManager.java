c9e941bff32a9e6ea0c7875d0585eb15e14c33b5
kperegrin <kperegrin@users.noreply.github.com>
2026-08-13 23:38:19 +0200

    v1.0.38: quitar Aikar JVM flags, dejar argumentos simples

--- HMCL/src/main/java/org/jackhuang/hmcl/server/ServerInstanceManager.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/server/ServerInstanceManager.java
@@ -83,16 +83,7 @@ public final class ServerInstanceManager {
             setting.setServerIp(quickJoin ? manifest.getServer().getAddress() : "");
             setting.setVersionIcon(VersionIconType.FABRIC);
             setting.setLauncherVisibility(LauncherVisibility.HIDE_AND_REOPEN);
-            // Aikar's G1GC flags for better FPS and lower GC pauses
-            setting.setJavaArgs(
-                    "-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200" +
-                    " -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch" +
-                    " -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M" +
-                    " -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4" +
-                    " -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90" +
-                    " -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32" +
-                    " -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1"
-            );
+            setting.setJavaArgs("");
             repository.saveVersionSetting(ServerLauncherConfig.INSTANCE_NAME);
         }
 
